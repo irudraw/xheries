183885b5daaaa6712714a8c7f97e0d33544e9527
irudraw <41246008+irudraw@users.noreply.github.com>
2018-07-29 02:22:57 -0500

    Add files via upload

---
+++ anime/SHINGEKI NO BAHAMUT VIRGIN SOUL/js.js
@@ -0,0 +1,33 @@
+myFunction([
+{
+"display": "01",
+"titulo": "SHINGEKI NO BAHAMUT VIRGIN SOUL",
+"subt": "Sub Esp.",
+"url": "'V1ZWb1UwMUhUa1ZpTTFwTlRXMW5lVmxzVWtaTk1ERklXVE5XYTFOR1ZqRmFSV2hhWkcxU2RHSkhkRTloYlU0eVZGVk5OR1F3TVRaWk0xcFBZV3hGZDFSSWNFSmtNREUyV1hwS1QxSkdSalJVYlhCT1pGZEtXVkZVUWxGTmJXZ3lXWHBPVWs5WFJrbFhibEpPVmtkT00xZHVhekZOUjFKVVRsUkNhMXA2TURrPStW'",
+"desc": "V1ZWb1UwMUhUa1ZpTTFwTlRXMW5lVmxzVWtaTk1ERklXVE5XYTFOR1ZqRmFSV2hhWkcxU2RHSkhkRTloYlU0eVZGVk5OR1F3TVRaWk0xcFBZV3hGZDFSSWNFSmtNREUyV1hwS1QxSkdSalJVYlhCT1pGZEtXVkZVUWxGTmJXZ3lXWHBPVWs5WFJrbFhibEpPVmtkT00xZHVhekZOUjFKVVRsUkNhMXA2TURrPStW",
+"onc": "'myVid'",
+"imag": "poster.png",
+"postervid": "'img/postervid.png'",
+},
+{
+"display": "02",
+"titulo": "SHINGEKI NO BAHAMUT VIRGIN SOUL",
+"subt": "Sub Esp.",
+"url": "'aHR0cDovL2h2MTgyLnR1LnR2L3ZpZDE1Ni8wLzAzNy82NTAvMDAzNzY1MDc2NS5tcDQ/aG9zdD1odjE4Mi50dS50dita'",
+"desc": "aHR0cDovL2h2MTgyLnR1LnR2L3ZpZDE1Ni8wLzAzNy82NTAvMDAzNzY1MDc2NS5tcDQ/aG9zdD1odjE4Mi50dS50dita",
+"onc": "'myVid'",
+"imag": "poster.png",
+"postervid": "'img/postervid.png'",
+},
+{
+"display": "03",
+"titulo": "SHINGEKI NO BAHAMUT VIRGIN SOUL",
+"subt": "Sub Esp.",
+"url": "'aHR0cDovL2h2MTgzLnR1LnR2L3ZpZDE1NC8wLzAzNy82NTcvMDAzNzY1NzQzMC5tcDQ/aG9zdD1odjE4My50dS50dita'",
+"desc": "aHR0cDovL2h2MTgzLnR1LnR2L3ZpZDE1NC8wLzAzNy82NTcvMDAzNzY1NzQzMC5tcDQ/aG9zdD1odjE4My50dS50dita",
+"onc": "'myVid'",
+"imag": "poster.png",
+"postervid": "'img/postervid.png'",
+},
+
+])
---
+++ anime/Sakurada reset/js.js
@@ -0,0 +1,14 @@
+myFunction([
+{
+"display": "01",
+"titulo": "Sakurada reset",
+"subt": "Sub Esp.",
+"url": "'WVVoU01HTkViM1pNTW1neVlsUkZNMDFIWTNWa1NGVjFaRWhaZG1SdGJHdE9ha1YyVFVNNGQwMTZZM1pPYWxGNVRIcEJkMDE2WXpKT1JFbDNUMVJaZFdKWVFUQlFNbWgyWXpOUk9XRklXblJOVkdOM1duazFNR1JUTlRCa1p6MDkrVA=='",
+"desc": "WVVoU01HTkViM1pNTW1neVlsUkZNMDFIWTNWa1NGVjFaRWhaZG1SdGJHdE9ha1YyVFVNNGQwMTZZM1pPYWxGNVRIcEJkMDE2WXpKT1JFbDNUMVJaZFdKWVFUQlFNbWgyWXpOUk9XRklXblJOVkdOM1duazFNR1JUTlRCa1p6MDkrVA==",
+"onc": "'myVid'",
+"imag": "poster.png",
+"postervid": "'img/postervid.png'",
+},
+
+
+])
